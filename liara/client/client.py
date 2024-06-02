5c58a8367543a57ba969468dd226afc5d86f56a9
amirali irvany <irvanyamirali@gmail.com>
2024-06-02 21:52:34 +0330

    Update client.py

--- liara/client/client.py
+++ liara/client/client.py
@@ -1,15 +1,51 @@
-import aiohttp
+import requests
+from typing import Optional
+
+
+from liara import errors
+
 
 class Client:
 
-    def __init__(self, name: str, timeout: float = 20, version: int = 2):
-        self.session = aiohttp.ClientSession(
-            base_url=self.base_url
-        )
+    def __init__(
+            self,
+            name: str,
+            token: Optional[str] = None,
+            timeout: Optional[int] = 20,
+            version: Optional[int] = 1
+    ):
         self.token = token
-        self.timeout = timeout
         self.version = version
+        self.timeout = timeout
+        self.session = requests.session()
 
     @property
     def base_url(self) -> str:
-        return 'https://api.iran.liara.ir/' + self.version
+        return f'https://api.iran.liara.ir/v{self.version}/'
+
+    def execute(self, service: str, method: str, data: Optional[dict] = None) -> requests.Response:
+        headers: dict = {
+            'Accept': 'application/json, text/plain, */*',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7,fa;q=0.6',
+            'Authorization': f'Bearer {self.token}',
+            'Origin': 'https://console.liara.ir',
+            'Referer': 'https://console.liara.ir/',
+            'User-Agent': (
+                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'
+            )
+        }
+        result = self.session.request(
+            method=method, url=self.base_url + service, headers=headers, timeout=self.timeout, json=data
+        )
+        if result.json().get('error') == 'Unauthorized.':
+            raise errors.Unauthorized('Unauthorized account or session!')
+
+        return result
+
+    def get_services(self):
+        return self.execute(service='projects', method='get')
+
+    def get_my_account(self):
+        return self.execute(service='me', method='get')
