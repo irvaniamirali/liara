8319773d4f7598b355aea737bc18c7aeb1b19cb0
AmirAli Irvany <irvanyamirali@gmail.com>
2024-06-15 12:03:51 +0330

    Update session.py

--- liara/method/session/session.py
+++ liara/method/session/session.py
@@ -1,7 +1,10 @@
+from os.path import exists
+import json
+
 import liara
 
 
-class Login:
+class Session:
 
     def login(self: "liara.Client", email: str, password: str):
         params: dict = {
@@ -10,3 +13,18 @@ class Login:
         }
         result = self.api.execute(service="login", method="post", data=params).json()
         self.api_token = result.get("api_token")
+        return result
+
+    def set_session(self, session_name: str):
+        session_filename = f"{session_name}.session"
+        if not exists(session_filename):
+            self.email = input("Enter your email: ")
+            self.password = input("Enter your password: ")
+            user_data = self.login(email=self.email, password=self.password)
+
+            with open(session_filename, "w") as session_file:
+                session_file.write(json.dumps(user_data))
+        else:
+            with open(session_filename, "r") as session_file:
+                result = eval(session_file.read())
+                self.api_token = result.get("api_token")
