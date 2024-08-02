0ffb43704d7a2c85f9e3728a82a3da6da33ca486
AmirAli Irvani <irvaniamirali@proton.me>
2024-08-02 22:13:43 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='liara-cloud',
-    version='1.0.0',
+    version='2.0',
     author='AmirAli Irvani',
     author_email='irvaniamirali@proton.me',
     description=(
