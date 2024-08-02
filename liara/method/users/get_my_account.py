680c2d3da82dd1de42050dcbd70815fc015d691e
AmirAli Irvani <irvaniamirali@proton.me>
2024-08-02 21:14:25 +0330

    Update get_my_account.py

--- liara/method/users/get_my_account.py
+++ liara/method/users/get_my_account.py
@@ -4,4 +4,4 @@ import liara
 class GetMyAccount:
 
     def get_my_account(self: "liara.Client"):
-        return self.api.execute(service="me", method="get")
+        return self.api.execute(service="me", method="GET")
