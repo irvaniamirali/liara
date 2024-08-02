db2fec2e171cac76a18522f6503920b5f1f2893a
AmirAli Irvani <irvaniamirali@proton.me>
2024-08-02 21:14:38 +0330

    Update errors.py

--- liara/errors/errors.py
+++ liara/errors/errors.py
@@ -1,3 +1,13 @@
 
-class Unauthorized(Exception):
-    pass
+class APIError(Exception):
+
+    def __init__(self, message, code=None):
+        self.message = message
+        self.code = code
+        super().__init__(self.message)
+
+    def __str__(self):
+        if self.code is not None:
+            return f"APIError {self.code}: {self.message}"
+        else:
+            return f"APIError: {self.message}"
